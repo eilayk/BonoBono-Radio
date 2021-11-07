6c8a0b7ec44978be07cff7aefb55955bf1b3ceee
Terricing <eilay.katsnelson@gmail.com>
2021-11-07 13:01:01 -0500

    disconnect if inactive

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ import asyncio
 import discord
 import youtube_dl
 
-# import opus
-
 from discord.ext import commands
 
 # Suppress noise about console usage from errors
@@ -24,11 +22,9 @@ ytdl_format_options = {
     "source_address": "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
-
 ffmpeg_options = {"options": "-vn"}
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
@@ -53,25 +49,16 @@ class YTDLSource(discord.PCMVolumeTransformer):
         filename = data["url"] if stream else ytdl.prepare_filename(data)
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
-
 class Music(commands.Cog):
     queue = []
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="shalome")
-    async def join(self, ctx):
-        """Joins a voice channel"""
-        if ctx.author.voice:
-            await ctx.author.voice.channel.connect()
-        else:
-            await ctx.send('shalome')
-
     @commands.command(name="goto")
     async def move(self, ctx, *channel: discord.VoiceChannel):
-                if ctx.voice_client is not None:
-                    return await ctx.voice_client.move_to(channel)
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
 
     @commands.command(name="sing")
     async def play(self, ctx, *, url):
@@ -83,23 +70,40 @@ class Music(commands.Cog):
             async with ctx.typing():
                 player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
                 ctx.voice_client.play(
-                    player, after=lambda e: asyncio.run(self.play_next(ctx)))
+                    player, after=lambda e: self.play_next(ctx))
             await ctx.send(f"Now singing: {player.title}")
 
-    async def play_next(self, ctx):
+    def play_next(self, ctx):
         if len(self.queue) >= 1:
             source = self.queue[0]
             del self.queue[0]
             if ctx.voice_client.is_playing():
                 ctx.voice_client.stop()
-            ctx.voice_client.play(source, after=lambda e: asyncio.run(self.play_next(ctx)))
-
+            ctx.voice_client.play(source, after=lambda e: self.play_next(ctx))
+        else:
+            asyncio.run_coroutine_threadsafe(self.check_idle(ctx), self.bot.loop)
+        
+    async def check_idle(self, ctx):
+        await asyncio.sleep(100)
+        if not ctx.voice_client.is_playing():
+            await ctx.voice_client.disconnect()
+                
     @commands.command(name="skip")
     async def skip(self, ctx):
         if len(self.queue) >= 1:
             await self.play_next(ctx)
         else:
             await ctx.send('No songs in queue!')
+
+    @commands.command()
+    async def pause(self, ctx):
+        if ctx.voice_client.is_playing() and not ctx.voice_client.is_paused():
+            ctx.voice_client.pause()
+    
+    @commands.command()
+    async def resume(self, ctx):
+        if ctx.voice_client.is_playing() and ctx.voice_client.is_paused():
+            ctx.voice_client.resume()
 
     @commands.command()
     async def volume(self, ctx, volume: int=None):
@@ -136,6 +140,14 @@ class Music(commands.Cog):
                 await ctx.send("Connect to voice channel")
                 raise commands.CommandError("Author not connected to a voice channel.")
 
+    @resume.before_invoke
+    @pause.before_invoke
+    @stop.before_invoke
+    async def ensure_connect(self, ctx):
+        if not ctx.author.voice:
+            await ctx.send("Not connected to voice channel!")
+            raise commands.CommandError("Author not connected to a voice channel.")
+
 
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or("bonobono "),
@@ -149,4 +161,4 @@ async def on_ready():
 
 
 bot.add_cog(Music(bot))
-bot.run("")
+bot.run("MzI4OTUzNzQ3ODE4NTQ1MTU5.WVFIDQ.E9wKcb5MraRgNC1bR2Igcqq2j9o")
